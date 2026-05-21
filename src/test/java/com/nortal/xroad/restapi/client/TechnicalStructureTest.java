ec114d96b4b014a03c802d9c3a58a75cb745228f
Mohamed Elbeltagy <60988715+melbeltagy@users.noreply.github.com>
2026-05-22 00:27:34 +0300

    feat: add architectural rules for service layer naming conventions, field injection, and DTOs

--- src/test/java/com/nortal/xroad/restapi/client/TechnicalStructureTest.java
+++ src/test/java/com/nortal/xroad/restapi/client/TechnicalStructureTest.java
@@ -2,7 +2,11 @@ package com.nortal.xroad.restapi.client;
 
 import static com.tngtech.archunit.base.DescribedPredicate.alwaysTrue;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.belongToAnyOf;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noFields;
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
 import com.tngtech.archunit.junit.AnalyzeClasses;
@@ -28,4 +32,31 @@ class TechnicalStructureTest {
         .ignoreDependency(alwaysTrue(), belongToAnyOf(
             com.nortal.xroad.restapi.client.config.ApplicationProperties.class
         ));
+
+    @ArchTest
+    static final ArchRule serviceLayerNamingConventions = classes()
+        .that().resideInAPackage("..service..")
+        .and().areNotInterfaces()
+        .and().areNotRecords()
+        .and().areNotEnums()
+        .and().areNotAnonymousClasses()
+        .and().areNotMemberClasses()
+        .should().haveSimpleNameEndingWith("Service")
+        .orShould().haveSimpleNameEndingWith("Mapper")
+        .orShould().haveSimpleNameEndingWith("Builder")
+        .orShould().haveSimpleNameEndingWith("Factory")
+        .orShould().haveSimpleNameEndingWith("Validator");
+
+    @ArchTest
+    static final ArchRule noFieldInjection = noFields()
+        .should().beAnnotatedWith(Autowired.class)
+        .because("Use constructor injection instead of field injection");
+
+    @ArchTest
+    static final ArchRule dtosShouldBeRecords = classes()
+        .that().resideInAPackage("..dto..")
+        .and().haveSimpleNameEndingWith("Dto")
+        .or().haveSimpleNameEndingWith("DTO")
+        .should().beRecords()
+        .because("DTOs should be immutable records");
 }
